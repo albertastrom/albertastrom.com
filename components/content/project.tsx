783b68043eadaaf4f6c075226d254caf97bcfa1f
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2024-03-11 14:18:45 -0400

    Added some projects
    Not finished...

--- components/content/project.tsx
+++ components/content/project.tsx
@@ -19,9 +19,9 @@ const Project: React.FC<ProjectProps> = ({ title, description, link, linkTitle, 
             <p className='text-md text-gray-500'>{description}</p>
             <a href={link} className="inline-block font-semibold text-gray-600 text-sm"><FaLink className="inline-block text-gray-600 text-sm mr-1"/>{linkTitle}</a>
         </div>
-        <div>
+        {/* <div>
             <img src={imageUrl} alt={title} className="w-24 h-24 object-cover px-1 py-1 rounded-lg"/>
-        </div>
+        </div> */}
         
     </div>
   );
--- components/content/projects.tsx
+++ components/content/projects.tsx
@@ -5,8 +5,18 @@ const Projects = () => {
   return (
     <div className="mt-8">
         <h2 className="text-xl font-semibold mb-4">Projects</h2>
-        <Project title="CRInteractive" description="Look what I did..." link="https://www.crinter.com" linkTitle="Link to project" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+        <Project title="CRInteractive" description="Web-app designed to quantify the rowing stroke, create a home for coach-athlete communication, and unify team goals. Built for our team at Community Rowing Inc. (CRI)." link="https://www.crinteractive.org" linkTitle="crinteractive.org" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+
+        <Project title="stud: Study Buddy" description="Web-app and Chrome extension designed to optimize focus and time management through coaching of quantified studying metrics." link="https://github.com/albertastrom/stud" linkTitle="github.com/albertastrom/stud" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+
+        <Project title="Room Rank" description="Program design to rank dorm rooms on campus based on objective and subjective quantities, to pick the numerically best room." link="https://github.com/albertastrom/stud" linkTitle="github.com/albertastrom/stud" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+
+        <Project title="Space Barrage" description="Program design to rank dorm rooms on campus based on objective and subjective quantities, to pick the numerically best room." link="https://github.com/albertastrom/stud" linkTitle="github.com/albertastrom/stud" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+
+        <Project title="ThoughtJot" description="Program design to rank dorm rooms on campus based on objective and subjective quantities, to pick the numerically best room." link="https://github.com/albertastrom/stud" linkTitle="github.com/albertastrom/stud" imageUrl="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"/>
+
     </div>
+    
   );
 }
 
