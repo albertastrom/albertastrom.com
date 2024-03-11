3b075044b7707ffcfac3f664c6a0c6831c53522c
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2024-03-11 10:45:37 -0400

    fixed width

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <div className="antialiased sm:bg-sky-400 sm:px-2.5 sm:py-2.5 max-sm:border-t-4 max-sm:border-t-sky-400 bg-slate-50">
       <div className="bg-slate-50 sm:min-h-[calc(100vh-1.25rem)] sm:rounded-xl shadow-sm p-4 sm:pt-8">
-        <div className="flex justify-center min-w-[calc(100vh-1rem)]"> 
+        <div className="max-w-screen-md mx-auto"> 
           <div className="">
 
             <Header/>
