d14385e609c02be46e8811ead84c6cca5ac6dd4e
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2024-06-07 19:36:33 -0400

    Custom 404 and slight emoji tweaks

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import Header from '../components/header/header'
+import Custom404 from '@/components/content/404';
+
+
+const notFound = () => {
+  return (
+    <div className="antialiased sm:bg-sky-400 sm:px-2.5 sm:py-2.5 max-sm:border-t-4 max-sm:border-t-sky-400 bg-slate-50">
+      <div className="bg-slate-50 sm:min-h-[calc(100vh-1.25rem)] sm:rounded-xl shadow-sm p-4 sm:pt-8">
+        <div className="max-w-screen-md mx-auto"> 
+          <div className="">
+
+            <Header/>
+            <Custom404/>
+            
+            
+
+            <div>
+              {/* <img
+                src="https://www.uncovercolorado.com/wp-content/uploads/2013/12/Aspen-Highlands-Bowl-Hike-800x400.jpg"
+              /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default notFound;
---
+++ components/content/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div className="mt-8 text-lg">
+
+      <h2 className="text-gray-600">404 - Page not found :(</h2>
+      
+    </div>
+  );
+}
+
+export default Custom404;
