24a9c1513d6da04949ba13597302dc6130530f37
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2024-06-13 13:29:31 -0400

    bubbles with technologies! padding could be nice

--- components/content/project.tsx
+++ components/content/project.tsx
@@ -8,15 +8,21 @@ interface ProjectProps {
     link: string;
     linkTitle: string;
     imageUrl: string;
+    technologies: string[];
 }
 
-const Project: React.FC<ProjectProps> = ({ title, description, link, linkTitle, imageUrl }) => {
+const Project: React.FC<ProjectProps> = ({ title, description, link, linkTitle, imageUrl, technologies = [] }) => {
   return (
     
     <div className="mb-4 py-1 px-2 rounded-lg border border-gray-200 shadow-lg flex justify-between">
         <div className='px-1 py-2'>
             <p className='font-semibold text-xl'>{title}</p>
             <p className='text-md text-gray-500'>{description}</p>
+            <div className='flex flex-wrap '>
+              {technologies.map((tech, index) => (
+                <span key={index} className="inline-block bg-gray-200 rounded-full px-2 text-sm font-semibold text-gray-700 mr-2 mb-2 mt-2">{tech}</span>
+              ))}
+            </div>
             <a href={link} target="_blank" className="inline-block font-semibold text-gray-600 text-sm"><FaLink className="inline-block text-gray-600 text-sm mr-1"/>{linkTitle}</a>
         </div>
         {/* <div>
