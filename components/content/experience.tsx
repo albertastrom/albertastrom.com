bef1bbd1366a21722438a080b7cdaf6018fb55a0
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2025-03-25 15:48:15 +0100

    content updates

--- components/content/experience.tsx
+++ components/content/experience.tsx
@@ -5,9 +5,9 @@ const Experience = () => {
   return (
     <div className="mt-8 px-4 py-2 rounded-lg bg-white border border-gray-200">
         <h2 className="text-xl font-semibold mb-4">Experience</h2>
-        <Position title="Machine Learning Intern" company="C10 Labs" timeframe="Summer 2024" description="" link="https://www.c10labs.com" linkTitle="c10labs.com"/>
+        <Position title="Machine Learning Intern" company="C10 Labs" timeframe="Summer 2024" description="Developing startup application review system using LangChain, React, and serverless functions for the C10 Labs accelerator program." link="https://www.c10labs.com" linkTitle="c10labs.com"/>
 
-        <Position title="CS Teaching Assistant" timeframe="Feb. 2023 - May 2024" company="Colby CS Department" description="CS231: Data Structures and Algorithms" link="https://cs.colby.edu/courses/S24/231/" linkTitle="cs.colby.edu"/>
+        <Position title="CS Teaching Assistant" timeframe="Feb. 2023 - May 2024" company="Colby CS Department" description="CS231: Data Structures and Algorithms, CS333: Programming Languages" link="https://cs.colby.edu/courses/S24/231/" linkTitle="cs.colby.edu"/>
 
         
 
