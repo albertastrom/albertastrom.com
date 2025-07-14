9cc6a00093141c6b1207f133554ddab95601fad3
Albert Astrom <83228526+albertastrom@users.noreply.github.com>
2025-07-14 10:57:14 +0200

    improved footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="mt-4 mb-2 text-center text-sm text-gray-400">
+    <footer className="text-center text-sm text-gray-400 mt-[-0.5rem]">
       Albert Astrom © 2003 - 2025
     </footer>
   );
--- components/layout/PageWrapper.tsx
+++ components/layout/PageWrapper.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 const PageWrapper: React.FC<Props> = ({ children }) => {
   return (
-    <div className="antialiased sm:bg-sky-400 sm:px-2.5 sm:py-2.5 max-sm:border-t-4 max-sm:border-t-sky-400 bg-slate-50">
-      <div className="bg-slate-50 sm:min-h-[calc(100vh-1.25rem)] sm:rounded-xl shadow-sm p-4 sm:pt-8">
-        <div className="max-w-screen-md mx-auto">
+    <div className="flex flex-col min-h-screen antialiased sm:bg-sky-400 sm:px-2.5 sm:py-2.5 max-sm:border-t-4 max-sm:border-t-sky-400 bg-slate-50">
+      <div className="flex-1 bg-slate-50 sm:min-h-[calc(100vh-1.25rem)] sm:rounded-xl shadow-sm p-4 sm:pt-8 flex flex-col">
+        <div className="max-w-screen-md mx-auto flex-1 flex flex-col">
           <Header />
-          {children}
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
       </div>
